8e63d5cd1229e694653f876b9edcbaa1f2dd0774
Tomasz Nosol <tomasz.nosol.programista@gmail.com>
2021-12-15 18:57:13 +0100

    Created nav.php

---
+++ Views/includes/nav.php
@@ -0,0 +1,13 @@
+<nav class="top-nav"> 
+    <ul> 
+        <li>
+            <a href = "<?php echo URLROOT; ?>/index">Strona główna</a>
+        </li>
+        <li>
+            <a href = "<?php echo URLROOT; ?>/index">Zaloguj się</a>
+        </li>
+        <li>
+            <a href = "<?php echo URLROOT; ?>/index">Zarejestruj się</a>
+        </li>
+    </ul>
+</nav>
